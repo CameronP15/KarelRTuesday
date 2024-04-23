ffc1c01777a8256677e84989daaa6088044549d5
CameronP15 <150259709+CameronP15@users.noreply.github.com>
2024-04-23 09:43:03 -0400

    .

---
+++ .Trash-1000/files/construction.rb
@@ -0,0 +1,28 @@
+#!/opt/local/bin/ruby
+#Copyright 2012 Joseph Bergin
+#License: Creative Commons Attribution-Noncommercial-Share Alike 3.0 United States License
+
+$graphical = true
+
+require_relative "stair_sweeper"
+require_relative "../karel/robota"
+
+# a task for a stair sweeper
+def task()
+  world = Robota::World
+  world.read_world("../karel/construction")
+  
+  karel = ConstructionRobot.new(7, 2, Construction::EAST, 5)
+  
+end
+
+if __FILE__ == $0
+  if $graphical
+     screen = window(8, 40) # (size, speed)
+     screen.run do
+       task
+     end
+   else
+     task
+   end
+end
