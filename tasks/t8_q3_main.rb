d55502064898cba388ae5a9d91471024b77f5e35
CameronP15 <150259709+CameronP15@users.noreply.github.com>
2024-04-24 10:33:20 -0400

    .

---
+++ tasks/t8_q3_main.rb
@@ -0,0 +1,25 @@
+#!/opt/local/bin/ruby
+#Copyright 2012 Joseph Bergin
+#License: Creative Commons Attribution-Noncommercial-Share Alike 3.0 United States License
+
+$graphical = true
+
+require_relative "question_1"
+require_relative "../karel/robota"
+
+def task()
+  
+  karel = T8Robot1.new(1,1 - 5, 5)
+  karel.move
+end
+
+if __FILE__ == $0
+  if $graphical
+     screen = window(8, 40) # (size, speed)
+     screen.run do
+       task
+     end
+   else
+     task
+   end
+end
---
+++ tasks/t8_robot_4.rb
@@ -0,0 +1,26 @@
+#Copyright 2010 Joseph Bergin
+#License: Creative Commons Attribution-Noncommercial-Share Alike 3.0 United States License
+
+
+require_relative "../karel/ur_robot"
+require_relative "../mixins/turner"
+# A class whose robots know how to sweep a short staircase of beepers
+class RobotFaceNorth < UrRobot
+  include Turner
+  def initialize(6, 6, SUD, 0, couleur jaune)
+    super(street, avenue, direction, beepers)
+  end
+  
+  # climb one stair
+  def line
+    put_beeper
+    move
+    put_beeper
+    move
+    put_beeper
+    move
+    put_beeper
+    move
+    put_beeper
+  end
+end
